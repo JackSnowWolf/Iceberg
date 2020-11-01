ecad8e9b9c038619e38e0203aa2ec03127d4a866
JACKCHEN96 <cchenwenjie0901@gmail.com>
2020-11-01 20:57:53 +0800

    Basic entity
    create entity discussed before

--- src/main/java/com/iceburg/IceburgApplication.java
+++ src/main/java/com/iceburg/IceburgApplication.java
@@ -1,4 +1,16 @@
 package com.iceburg;
 
-public class IceburgApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class IceburgApplication extends SpringBootServletInitializer {
+    public static void main(String[] args){
+        SpringApplication.run(IceburgApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
+        return applicationBuilder.sources(IceburgApplication.class);
+    }
 }
---
+++ src/main/java/com/iceburg/entity/Group.java
@@ -0,0 +1,31 @@
+package com.iceburg.entity;
+
+public class Group {
+    private Integer id;
+    private String groupname;
+    private Integer managerid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getGroupname() {
+        return groupname;
+    }
+
+    public void setGroupname(String groupname) {
+        this.groupname = groupname;
+    }
+
+    public Integer getManagerid() {
+        return managerid;
+    }
+
+    public void setManagerid(Integer managerid) {
+        this.managerid = managerid;
+    }
+}
