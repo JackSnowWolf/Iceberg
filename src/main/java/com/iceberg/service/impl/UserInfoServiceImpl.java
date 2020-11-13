ffaa49a20d5c25c101ee28ae5ab7b8bb3b2f6b1c
mark2447 <54239290+mark2447@users.noreply.github.com>
2020-11-14 00:29:20 +0800

    Add files via upload

--- src/main/java/com/iceberg/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/iceberg/service/impl/UserInfoServiceImpl.java
@@ -1,11 +1,13 @@
 package com.iceberg.service.impl;
 
 import com.iceberg.dao.UserInfoMapper;
+import com.iceberg.entity.Group;
 import com.iceberg.entity.Role;
 import com.iceberg.entity.UserInfo;
 import com.iceberg.service.UserInfoService;
 import com.iceberg.utils.PageModel;
 import com.iceberg.utils.Result;
+import com.iceberg.utils.ResultUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -13,27 +15,38 @@ import java.util.List;
 
 @Service
 public class UserInfoServiceImpl implements UserInfoService {
+
     @Resource
     private UserInfoMapper userInfoMapper;
 
     @Override
     public int add(UserInfo userInfo) {
-        return 0;
+        //add user
+        int result = userInfoMapper.add(userInfo);
+        System.out.println(userInfo);
+        if (userInfo.getRoleid() == 2) {
+            //if the user is group manager, then set the managerid
+            Group newGroup = new Group();
+            newGroup.setManagerid(userInfo.getId());
+            int r = userInfoMapper.addGroupId(newGroup);
+
+            //successful, then bind members to this user
+            if (r == 1) {
+                userInfo.setGroupid(newGroup.getId().toString());
+                result = userInfoMapper.update(userInfo);
+            }
+        }
+        return result;
     }
 
     @Override
     public int update(UserInfo userInfo) {
-        return 0;
-    }
-
-    @Override
-    public boolean userIsExisted(UserInfo userInfo) {
-        return userInfoMapper.userIsExisted(userInfo) > 0 ? true : false;
+        return userInfoMapper.update(userInfo);
     }
 
     @Override
     public int delete(String id) {
-        return 0;
+        return userInfoMapper.delete(id);
     }
 
     @Override
@@ -42,33 +55,54 @@ public class UserInfoServiceImpl implements UserInfoService {
     }
 
     @Override
+    public boolean userIsExisted(UserInfo userInfo) {
+        return userInfoMapper.userIsExisted(userInfo) > 0 ? true : false;
+    }
+
+    @Override
     public Result getUsersByWhere(PageModel<UserInfo> model) {
-        return null;
+        try {
+            List<UserInfo> users = userInfoMapper.getUsersByWhere(model);
+            if (users.size() >= 0) {
+                Result<UserInfo> result = ResultUtil.success(users);
+                result.setTotal(userInfoMapper.getToatlByWhere(model));
+                if (result.getTotal() == 0) {
+                    result.setMsg("No related data");
+                } else {
+                    result.setMsg("Data fetched successfully");
+                }
+                return result;
+            } else {
+                return ResultUtil.unSuccess("no satisfied condition found！");
+            }
+        } catch (Exception e) {
+            return ResultUtil.error(e);
+        }
     }
 
     @Override
     public List<Role> getAllRoles() {
-        return null;
+        return userInfoMapper.getAllRoles();
     }
 
     @Override
     public int addRole(Role role) {
-        return 0;
+        return userInfoMapper.addRole(role);
     }
 
     @Override
     public int updateRole(Role role) {
-        return 0;
+        return userInfoMapper.updateRole(role);
     }
 
     @Override
     public int deleteRole(String id) {
-        return 0;
+        return userInfoMapper.deleteRole(id);
     }
 
     @Override
     public Role getRoleById(String id) {
-        return null;
+        return userInfoMapper.getRoleById(id);
     }
 
 }
