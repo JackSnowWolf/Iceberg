9cc77e29c588cde10debc680efc665c4841cf84e
mark2447 <hwj97055@gmail.com>
2020-11-15 12:12:59 +0800

    Feature 20201114 weijie (#10)
    * small modification
    
    * some test file for OAuth with Github account
    test file not uploaded due to private credentials
    some changes to controller and pom.xml
    Define HttpClientUtils, help to test
    
    * ``
    
    * OAuth successfully implemented

--- src/main/java/com/iceberg/configs/SessionInterceptor.java
+++ src/main/java/com/iceberg/configs/SessionInterceptor.java
@@ -21,8 +21,9 @@ public class SessionInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("token: "+request.getHeader("token"));
-        System.out.println(request.getRequestURL());
+//        System.out.println("token: "+request.getHeader("token"));
+//        System.out.println(request.getRequestURL());
+        System.out.println("session preHandler runs *******************");
         HttpSession session = request.getSession();
 
 
@@ -41,6 +42,7 @@ public class SessionInterceptor implements HandlerInterceptor {
                     userInfo.setUsername(userinfoStr.split("_")[0]);
                     userInfo = userInfoController.getUserInfo(userInfo);
                     userInfoController.setSessionUserInfo(userInfo,session);
+                    System.out.println("session prehandler return true");
                     return true;
                 }
             }
@@ -48,20 +50,25 @@ public class SessionInterceptor implements HandlerInterceptor {
 
         //mobile client skip
         if ("client".equals(request.getHeader("token"))){
+            System.out.println("session prehandler return true");
             return true;
         }
         String uri = request.getRequestURI();
+        System.out.println("URI IS  : " + uri);
 //        System.out.println("session:"+uri);
         //login page or static resource, don't intercept
-        if("/".equals(uri) || "/login.html".equals(uri) || "/login.do".equals(uri) || uri.contains("/static/")){
+        if("/".equals(uri) || "/login.html".equals(uri) || "/login.do".equals(uri) || uri.contains("/static/") || uri.startsWith("/oauth")){
 //            System.out.println("---let them go---");
         }else {
             //no login page, check session, no session then redirect to login page
             if (session.getAttribute(Config.CURRENT_USERNAME) == null){
                 response.sendRedirect("/login.html");
+                System.out.println("session prehandler return false!!!!!");
                 return false;
             }
         }
+
+        System.out.println("session prehandler return true");
         return true;
     }
 
@@ -73,11 +80,16 @@ public class SessionInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 //        System.out.println(request.getRequestURI());
+        System.out.println("sessionInterceptor After Completion++++++++++++++++++processing");
         String uri = request.getRequestURI();
-        if("/logout".equals(uri) && request.getSession().getAttribute(Config.CURRENT_USERNAME) == null){
-           response.sendRedirect("/login.html");
+        if("/logout".equals(uri) || "/".equals(uri) || "/login.html".equals(uri) || uri.startsWith("/static") || uri.startsWith("/oauth")) {
+            //avoid repeating redirecting
+            return;
         }
-
+        if(request.getSession().getAttribute(Config.CURRENT_USERNAME) == null){
+           response.sendRedirect("/login.html");
+           return;
+        }
     }
 
     /**
