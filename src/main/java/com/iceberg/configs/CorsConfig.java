458d8d545891ce6dcc938599839b2dae6eea0352
mark2447 <hwj97055@gmail.com>
2020-11-15 18:19:37 +0800

    code style test

--- src/main/java/com/iceberg/configs/CorsConfig.java
+++ src/main/java/com/iceberg/configs/CorsConfig.java
@@ -8,24 +8,26 @@ import org.springframework.web.filter.CorsFilter;
 
 
 /**
-* description: cross-Origin Resource Sharing
-* @author Weijie Huang
-* @date 2020/11/13
-*/
+ * description: cross-Origin Resource Sharing
+ *
+ * @author Weijie Huang
+ * @date 2020/11/13
+ */
 @Configuration
 public class CorsConfig {
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*"); // 1 any Origin
-        corsConfiguration.addAllowedHeader("*"); // 2 any header
-        corsConfiguration.addAllowedMethod("*"); // 3 any method
-        return corsConfiguration;
-    }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); // 4
-        return new CorsFilter(source);
-    }
+  private CorsConfiguration buildConfig() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.addAllowedOrigin("*"); // 1 any Origin
+    corsConfiguration.addAllowedHeader("*"); // 2 any header
+    corsConfiguration.addAllowedMethod("*"); // 3 any method
+    return corsConfiguration;
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", buildConfig()); // 4
+    return new CorsFilter(source);
+  }
 }
