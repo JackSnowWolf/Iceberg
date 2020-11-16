85b56731c2d396a738c72b2cf75e99bc8113e0fe
Zeyang-Chen <54789055+Zeyang-Chen@users.noreply.github.com>
2020-11-16 22:37:18 +0800

    Update PrivilegeMapperTest.java

--- src/test/java/com/iceberg/dao/PrivilegeMapperTest.java
+++ src/test/java/com/iceberg/dao/PrivilegeMapperTest.java
@@ -1,6 +1,5 @@
 package com.iceberg.dao;
 
-
 import com.iceberg.entity.Privilege;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -9,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 //@MapperScan(basePackages = {"com.iceberg.dao","resources.mappers"})
@@ -21,29 +22,34 @@ public class PrivilegeMapperTest {
 
 
     @Test
-    public void getPrivilegeByRoleId() {
-        logger.info("getPrivilegeByRoleId test");
-        List<Privilege> all = privilegeMapper.getPrivilegeByRoleid(1);
-        System.out.println(all.get(0).toString());
-        logger.info("getPrivilegeByRoleId test success");
-
-    }
-
-    @Test
     public void addDefaultPrivilegesWhenAddRoleTest() {
         logger.info("addDefaultPrivilegesWhenAddRoleTest test");
-        int res = privilegeMapper.addDefaultPrivilegesWhenAddRole("1");
+        int res = privilegeMapper.addDefaultPrivilegesWhenAddRole("3");
         System.out.println(res);
+        assertEquals(6, res);
         logger.info("addDefaultPrivilegesWhenAddRoleTest test success");
 
 
     }
 
     @Test
+    public void getPrivilegeByRoleId() {
+        logger.info("getPrivilegeByRoleId test");
+        List<Privilege> all = privilegeMapper.getPrivilegeByRoleid(1);
+//        System.out.println(all.get(0).toString());
+        assertEquals(10, all.size());
+        logger.info("getPrivilegeByRoleId test success");
+
+    }
+
+
+
+    @Test
     public void delPrivilegesWenDelRoleTest() {
         logger.info("delPrivilegesWenDelRoleTest test");
-        int res = privilegeMapper.delPrivilegesWenDelRole("1");
+        int res = privilegeMapper.delPrivilegesWenDelRole("3");
         System.out.println(res);
+        assertEquals(6, res);
         logger.info("delPrivilegesWenDelRoleTest test success");
     }
 
