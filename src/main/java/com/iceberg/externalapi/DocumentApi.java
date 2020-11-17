f0ee66606fb26cc0932d0fd3d9b4e309ee58a028
JACKCHEN96 <cchenwenjie0901@gmail.com>
2020-11-17 20:55:04 +0800

    delete ocr part

--- src/main/java/com/iceberg/externalapi/DocumentApi.java
+++
@@ -1,8 +0,0 @@
-package com.iceberg.externalapi;
-
-public interface DocumentApi {
-    void updateDocumentDetails();
-    void getAllDocuments();
-    void getSingleDocuments();
-    void deleteSingleDocument();
-}
