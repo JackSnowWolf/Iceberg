6b4df2aa126801fc16b881dd41a0c7d43fa5d47b
JACKCHEN96 <cchenwenjie0901@gmail.com>
2020-12-01 01:02:49 +0800

    annotate paypal and processing tests for saving from autotest

--- src/test/java/com/iceberg/externalapi/ProcessingImplTest.java
+++ src/test/java/com/iceberg/externalapi/ProcessingImplTest.java
@@ -1,7 +1,6 @@
 package com.iceberg.externalapi;
 
 import com.alibaba.fastjson.JSONObject;
-import com.google.gson.JsonObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -12,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import javax.annotation.Resource;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.Base64;
 
@@ -21,51 +19,51 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ProcessingImplTest {
-    private Logger logger = LoggerFactory.getLogger(ProcessingImplTest.class);
-    private File testImageFile;
-    private String filename;
-
-    @Resource
-    private ProcessingApi processingApi;
-
-    @BeforeEach
-    void init() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        URL result = classLoader.getResource("images/invoice-test-01.png");
-        assertNotNull(result);
-        filename=result.getFile();
-        testImageFile = new File(result.getFile());
-    }
-
-    @Test
-    public void processDocumentURLTest(){
-        ResponseEntity<String> response=processingApi.processDocumentURL("https://s3.amazonaws.com/isc.pricescout.media/receipts/be04115eb8824719914cfbcfd7e792c2.jpg");
-        JSONObject test= JSONObject.parseObject(response.getBody());
-        String money=test.getString("total");
-        String vendorname=test.getJSONObject("vendor").getString("name");
-        String vendoraddr=test.getJSONObject("vendor").getString("address");
-        String duedate=test.getString("due_date");
-        logger.info(money);
-        logger.info(vendorname);
-        logger.info(vendoraddr);
-        logger.info(duedate);
-        assertNotNull(response);
-    }
-
-    @Test
-    public void processDocumentBase64Test(){
-        byte[] fileBytes = getFileBytes(testImageFile.getAbsolutePath());
-        String encoded= Base64.getEncoder().encodeToString(fileBytes);
-        ResponseEntity<String> response=processingApi.processDocumentBase64(encoded);
-        JSONObject test= JSONObject.parseObject(response.getBody());
-        String money=test.getString("total");
-        String vendorname=test.getJSONObject("vendor").getString("name");
-        String vendoraddr=test.getJSONObject("vendor").getString("address");
-        String duedate=test.getString("due_date");
-        logger.info(money);
-        logger.info(vendorname);
-        logger.info(vendoraddr);
-        logger.info(duedate);
-        assertNotNull(response);
-    }
+//    private Logger logger = LoggerFactory.getLogger(ProcessingImplTest.class);
+//    private File testImageFile;
+//    private String filename;
+//
+//    @Resource
+//    private ProcessingApi processingApi;
+//
+//    @BeforeEach
+//    void init() {
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        URL result = classLoader.getResource("images/invoice-test-01.png");
+//        assertNotNull(result);
+//        filename=result.getFile();
+//        testImageFile = new File(result.getFile());
+//    }
+//
+//    @Test
+//    public void processDocumentURLTest(){
+//        ResponseEntity<String> response=processingApi.processDocumentURL("https://s3.amazonaws.com/isc.pricescout.media/receipts/be04115eb8824719914cfbcfd7e792c2.jpg");
+//        JSONObject test= JSONObject.parseObject(response.getBody());
+//        String money=test.getString("total");
+//        String vendorname=test.getJSONObject("vendor").getString("name");
+//        String vendoraddr=test.getJSONObject("vendor").getString("address");
+//        String duedate=test.getString("due_date");
+//        logger.info(money);
+//        logger.info(vendorname);
+//        logger.info(vendoraddr);
+//        logger.info(duedate);
+//        assertNotNull(response);
+//    }
+//
+//    @Test
+//    public void processDocumentBase64Test(){
+//        byte[] fileBytes = getFileBytes(testImageFile.getAbsolutePath());
+//        String encoded= Base64.getEncoder().encodeToString(fileBytes);
+//        ResponseEntity<String> response=processingApi.processDocumentBase64(encoded);
+//        JSONObject test= JSONObject.parseObject(response.getBody());
+//        String money=test.getString("total");
+//        String vendorname=test.getJSONObject("vendor").getString("name");
+//        String vendoraddr=test.getJSONObject("vendor").getString("address");
+//        String duedate=test.getString("due_date");
+//        logger.info(money);
+//        logger.info(vendorname);
+//        logger.info(vendoraddr);
+//        logger.info(duedate);
+//        assertNotNull(response);
+//    }
 }
