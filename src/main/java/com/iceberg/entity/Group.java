b1ca9a0867b61eb2f6f2db47348e2641c987c1a2
mark2447 <hwj97055@gmail.com>
2020-12-03 13:50:08 +0800

    test coverage，add lombok plugins (#71)

--- src/main/java/com/iceberg/entity/Group.java
+++ src/main/java/com/iceberg/entity/Group.java
@@ -1,31 +1,34 @@
 package com.iceberg.entity;
 
+import lombok.Data;
+
+@Data
 public class Group {
   private Integer id;
   private String groupname;
   private Integer managerid;
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getGroupname() {
-    return groupname;
-  }
-
-  public void setGroupname(String groupname) {
-    this.groupname = groupname;
-  }
-
-  public Integer getManagerid() {
-    return managerid;
-  }
-
-  public void setManagerid(Integer managerid) {
-    this.managerid = managerid;
-  }
+//
+//  public Integer getId() {
+//    return id;
+//  }
+//
+//  public void setId(Integer id) {
+//    this.id = id;
+//  }
+//
+//  public String getGroupname() {
+//    return groupname;
+//  }
+//
+//  public void setGroupname(String groupname) {
+//    this.groupname = groupname;
+//  }
+//
+//  public Integer getManagerid() {
+//    return managerid;
+//  }
+//
+//  public void setManagerid(Integer managerid) {
+//    this.managerid = managerid;
+//  }
 }
