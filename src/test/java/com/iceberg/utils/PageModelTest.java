c1b2c84fa9b12b83ce4df637936cf4753edb40de
JACKCHEN96 <cchenwenjie0901@gmail.com>
2020-12-03 19:12:54 +0800

    Update PageModelTest.java

--- src/test/java/com/iceberg/utils/PageModelTest.java
+++ src/test/java/com/iceberg/utils/PageModelTest.java
@@ -2,11 +2,22 @@ package com.iceberg.utils;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PageModelTest {
     @Test
     public void equalAndSupportTest() {
+        PageModel pageModel=new PageModel(1,1);
+        PageModel pageModel1=new PageModel(2,2);
+        pageModel.setBeginIndex(1);
+        int beginIndex=pageModel.getBeginIndex();
+        assertNotNull(beginIndex);
+        pageModel.setCurrentPageNo(1);
+        int currentPageNo=pageModel.getCurrentPageNo();
+        assertNotNull(currentPageNo);
+        pageModel.setData(2);
+        assertFalse(pageModel.equals(pageModel1));
+        assertTrue(pageModel.equals(pageModel));
 
     }
 }
