a886ad56e3540eb2854b70c703645405e60a8202
Chong Hu <1178869226@qq.com>
2020-12-03 20:25:33 -0500

    [REVISE] lombok configuration (#72)
    * [REVISE] lombok configuration
    
    * [ADD] Report From CI
    
    Co-authored-by: GitHub <action@github.com>

--- src/main/java/com/iceberg/entity/InvoiceDetail.java
+++ src/main/java/com/iceberg/entity/InvoiceDetail.java
@@ -1,11 +1,14 @@
 package com.iceberg.entity;
 
 import lombok.Data;
+import lombok.Generated;
 
 @Data
+@Generated
 public class InvoiceDetail {
-    private Float money;
-    private String vendorname;
-    private String vendoraddr;
-    private String duedate;
+
+  private Float money;
+  private String vendorname;
+  private String vendoraddr;
+  private String duedate;
 }
