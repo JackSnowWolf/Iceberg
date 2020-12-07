542ba2f06101c4d040e468952a2210e227d8fa7b
JACKCHEN96 <cchenwenjie0901@gmail.com>
2020-12-07 14:12:36 +0800

    row back "external api" test

--- src/test/java/com/iceberg/externalapi/ProcessingForRequestImplTest.java
+++ src/test/java/com/iceberg/externalapi/ProcessingForRequestImplTest.java
@@ -19,34 +19,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ProcessingForRequestImplTest {
-//    private Logger logger = LoggerFactory.getLogger(ProcessingForRequestImplTest.class);
-//    private File testImageFile;
-//
-//    @Resource
-//    private ProcessingForRequestImpl processingForRequest;
-//
-//    @BeforeEach
-//    void init() {
-//        ClassLoader classLoader = getClass().getClassLoader();
-//        URL result = classLoader.getResource("images/invoice-test-01.png");
-//        assertNotNull(result);
-//        testImageFile = new File(result.getFile());
-//    }
-//
-//    @Test
-//    public void parseFromDocumentBase64Test(){
-//        byte[] fileBytes = getFileBytes(testImageFile.getAbsolutePath());
-//        String encoded= Base64.getEncoder().encodeToString(fileBytes);
-//        InvoiceDetail invoiceDetail=processingForRequest.parseFromDocumentBase64(encoded);
-//        logger.info(invoiceDetail.toString());
-//        assertEquals("4520.0",invoiceDetail.getMoney().toString());
-//    }
-//
-//    @Test
-//    public void parseFromDocumentURLTest(){
-//        InvoiceDetail invoiceDetail=processingForRequest.parseFromDocumentURL("https://s3.amazonaws.com/isc.pricescout.media/receipts/be04115eb8824719914cfbcfd7e792c2.jpg");
-//        logger.info(invoiceDetail.toString());
-//        assertNotNull(invoiceDetail);
-//    }
+    private Logger logger = LoggerFactory.getLogger(ProcessingForRequestImplTest.class);
+    private File testImageFile;
+
+    @Resource
+    private ProcessingForRequestImpl processingForRequest;
+
+    @BeforeEach
+    void init() {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL result = classLoader.getResource("images/invoice-test-01.png");
+        assertNotNull(result);
+        testImageFile = new File(result.getFile());
+    }
+
+    @Test
+    public void parseFromDocumentBase64Test() {
+        byte[] fileBytes = getFileBytes(testImageFile.getAbsolutePath());
+        String encoded = Base64.getEncoder().encodeToString(fileBytes);
+        InvoiceDetail invoiceDetail = processingForRequest.parseFromDocumentBase64(encoded);
+        logger.info(invoiceDetail.toString());
+        assertEquals("4520.0", invoiceDetail.getMoney().toString());
+    }
+
+    @Test
+    public void parseFromDocumentURLTest() {
+        InvoiceDetail invoiceDetail = processingForRequest.parseFromDocumentURL("https://s3.amazonaws.com/isc.pricescout.media/receipts/be04115eb8824719914cfbcfd7e792c2.jpg");
+        logger.info(invoiceDetail.toString());
+        assertNotNull(invoiceDetail);
+    }
 
 }
